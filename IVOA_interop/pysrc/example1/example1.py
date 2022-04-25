3cd2992aad69e3620098265fa948b759657289aa
HEINL HENDRIK <heinl@g-vo.org>
2022-04-25 22:20:59 +0200

    switched service due to astropy reluctance to accept VO tables coming from the GAVO obscore service

--- IVOA_interop/pysrc/example1/example1.py
+++ IVOA_interop/pysrc/example1/example1.py
@@ -4,16 +4,16 @@
 # A demo program to show how a TAP query in PyVO works. 
 
 import pyvo
-
+import astropy 
 import sys
 import warnings
 
 if not sys.warnoptions:
-warnings.simplefilter("ignore")
+    warnings.simplefilter("ignore")
 
 
-service = pyvo.dal.TAPService ("http://dc.zah.uni-heidelberg.de/tap")
-result = service.search ("SELECT TOP 1 * FROM ivoa.obscore")
+service = pyvo.dal.TAPService ("http://cdsarc.cds.unistra.fr/saadavizier.tap/tap")
+result = service.search("SELECT TOP 1 * FROM obscore")
 
 result.votable.to_xml("result.vot")
 
